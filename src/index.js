d29108295242faa36e22d85834823afbdb3e5c45
JamesMGithire <105742325+JamesMGithire@users.noreply.github.com>
2022-08-16 20:15:29 +0300

    trasition time 2 to 1.5 changed the time for transitions to 1.5sec

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 let hrBar = document.getElementById("nomadHr");
-hrBar.style.transition = "all 2s"
+hrBar.style.transition = "all 1.5s";
 let home = document.getElementById("home");
 home.addEventListener("click", (e) => {
-    console.log(hrBar.style.left)
-        hrBar.style.width = "16%";
-        hrBar.style.left = "0%";
+  console.log(hrBar.style.left);
+  hrBar.style.width = "16%";
+  hrBar.style.left = "0%";
 });
 
 let rental = document.getElementById("rental");
 rental.addEventListener("click", (e) => {
-    console.log(hrBar.style.left);
-    hrBar.style.width = "17%";
-    hrBar.style.left = "16%";
+  console.log(hrBar.style.left);
+  hrBar.style.width = "17%";
+  hrBar.style.left = "16%";
 });
 
 let login = document.getElementById("login");
 login.addEventListener("click", (e) => {
-    console.log(hrBar.style.left);
-    hrBar.style.width = "18.7%";
-    hrBar.style.left = "33.5%";
-})
+  console.log(hrBar.style.left);
+  hrBar.style.width = "18.7%";
+  hrBar.style.left = "33.5%";
+});
 
 let sign = document.getElementById("sign-up");
 sign.addEventListener("click", (e) => {
-    console.log(hrBar.style.left);
-    hrBar.style.width = "17%";
-    hrBar.style.left = "52.7%";
-})
+  console.log(hrBar.style.left);
+  hrBar.style.width = "17%";
+  hrBar.style.left = "52.7%";
+});
