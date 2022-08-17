a6666c9c8af2b1ed011fd0ea46135b58b4eb0a83
JamesMGithire <105742325+JamesMGithire@users.noreply.github.com>
2022-08-17 09:28:11 +0300

    Html body size and cursor Made html

--- src/index.js
+++ src/index.js
@@ -2,28 +2,24 @@ let hrBar = document.getElementById("nomadHr");
 hrBar.style.transition = "all 1.5s";
 let home = document.getElementById("home");
 home.addEventListener("click", (e) => {
-  console.log(hrBar.style.left);
   hrBar.style.width = "16%";
   hrBar.style.left = "0%";
 });
 
 let rental = document.getElementById("rental");
 rental.addEventListener("click", (e) => {
-  console.log(hrBar.style.left);
   hrBar.style.width = "17%";
   hrBar.style.left = "16%";
 });
 
 let login = document.getElementById("login");
 login.addEventListener("click", (e) => {
-  console.log(hrBar.style.left);
   hrBar.style.width = "18.7%";
   hrBar.style.left = "33.5%";
 });
 
 let sign = document.getElementById("sign-up");
 sign.addEventListener("click", (e) => {
-  console.log(hrBar.style.left);
   hrBar.style.width = "17%";
   hrBar.style.left = "52.7%";
 });
